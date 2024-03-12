96fa3c7b081135c5e8f43c938c42762d84580102
Baker Alshaban <baker.alshaban@dgiusa.net>
2024-03-12 14:48:01 -0500

    initial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter } from "react-router-dom"
+
+import { BrowserRouter } from "react-router-dom"
+
+
+const App = () => {
+
+  return (
+    <div>
+      Portfolio
+    </div>
+  )
+}
+
+export default App
