fd20a889023bb4b554932b3b317eee6ca99a6f82
Baker Alshaban <baker.alshaban@dgiusa.net>
2024-03-19 12:38:50 -0500

    about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,10 +14,10 @@ const ServiceCard = ({ index, title, icon }) => {
         className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
       >
         <div
-          options={{max: 45, scale: 1, speed: 400}}
+          options={{ max: 45, scale: 1, speed: 400 }}
           className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
         >
-          <img src={icon} alt='web-development' className='w-16 h-16 object-contain'/>
+          <img src={icon} alt='web-development' className='w-16 h-16 object-contain' />
 
           <h3 className='text-white text-[20px] font-bold text-center'>
             {title}
@@ -40,11 +40,9 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        I'm a skilled IT Professional with experience in TypeScript and
+        JavaScript also have knowledge in frameworks like React and Node.js. 
+        I'm a quick learner and love leaning new things!
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
